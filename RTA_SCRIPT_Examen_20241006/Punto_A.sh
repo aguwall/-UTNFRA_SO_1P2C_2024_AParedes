72706f73367f3fd372ef07ca15f66206fc483da5
aguwall <agustinalejoparedes@gmail.com>
2024-10-08 02:22:12 +0000

    ADD: Se agrego estructura del PUNTO A(estructura simétrica)

---
+++ RTA_SCRIPT_Examen_20241006/Punto_A.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Creacion de estructura"
+
+ sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,pparcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
+
+echo
+echo "mostrando carpetas y sus subcarpetas"
+
+sudo tree /Examenes-UTN
+echo
+
