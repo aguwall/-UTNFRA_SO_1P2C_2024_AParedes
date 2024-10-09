86ddd8367b1ae9395e8dbf6abd107168ef6faf6f
aguwall <agustinalejoparedes@gmail.com>
2024-10-09 18:04:40 +0000

    ADD: se agrego estructura simetrica, punto D

---
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Creacion de Estructura Asimétrica"
+
+
+sudo mkdir -p /Estructura_Asimetrica/{{correo,clientes}/cartas_{1..100},correo/carteros_{1..10}}
+
+
+
+echo "mostrando el resultado"
+
+tree /Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --columns 4
+
