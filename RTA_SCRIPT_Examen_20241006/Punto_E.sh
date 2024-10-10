683cc26bfb67a9fe974661be9dd9a09887c7bed9
aguwall <agustinalejoparedes@gmail.com>
2024-10-10 02:50:47 +0000

    ADD: modificacion de un texto

--- RTA_SCRIPT_Examen_20241006/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_E.sh
@@ -6,4 +6,6 @@ cat /proc/meminfo | grep -i "memtotal" > UTNFRA_SO_1P2C_2024_AParedes/RTA_ARCHIV
 echo "Obtengo informacion del chasis:"
 sudo dmidecode -t chassis | grep -i "manufacturer" >> UTNFRA_SO_1P2C_2024_AParedes/RTA_ARCHIVOS_Examen_20241006/Filtro_Basico.txt
 
+echo
+
 
